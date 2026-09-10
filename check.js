ed4049abc2ac36c54dd1dee763d0146b839c045b
nedpearson <nedpearson@gmail.com>
2026-09-10 17:32:30 -0500

    chore: remove temp scripts

--- check.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const file = 'apps/marketing/src/lib/services/businessStore.ts';
-let content = fs.readFileSync(file, 'utf8');
-console.log(content);
--- fix.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const file = 'apps/marketing/src/components/vowos/settings/tabs/AIModelSettingsTab.tsx';
-let content = fs.readFileSync(file, 'utf8');
-content = content.replace('btnSecondary, inputCls, labelCls', 'btnSecondary, inputCls');
-content = content.replace(/className=\{labelCls\}/g, 'className="block text-xs font-semibold text-stone-700 mb-1"');
-fs.writeFileSync(file, content);
